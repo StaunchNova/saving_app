0c345bdb2108da5f5b36527b7564964e7ce203fb
Paschal <paschalkedoo@gmail.com>
2022-04-04 22:21:07 +0100

    setting up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const FlutterBankApp());
@@ -6,6 +7,23 @@ void main() {
 
 class FlutterBankApp extends StatelessWidget {
   const FlutterBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      debugShowCheckedModeBanner: true,
+      home: const FlutterBankSplash(),
+    );
+  }
+}
+
+class FlutterBankSplash extends StatelessWidget {
+  const FlutterBankSplash({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
