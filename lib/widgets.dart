b07cb011217a92760ca01c0015f44399717bf82c
Paschal <paschalkedoo@gmail.com>
2022-04-09 00:27:49 +0100

    added username and password field

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -44,6 +44,8 @@ class FlutterBankLogin extends StatefulWidget {
 }
 
 class _FlutterBankLoginState extends State<FlutterBankLogin> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,11 +87,75 @@ class _FlutterBankLoginState extends State<FlutterBankLogin> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: const [
-                    Text(
+                  children: [
+                    const Text(
                       "Sign Into Your Bank Account",
                       textAlign: TextAlign.center,
                       style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(50)),
+                      padding: const EdgeInsets.all(5),
+                      child: TextField(
+                        onChanged: (text) {
+                          setState(() {});
+                        },
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                              left: 20, bottom: 11, top: 11, right: 15),
+                          hintText: "Email",
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Utils.mainThemeColor,
+                          ),
+                        ),
+                        style: const TextStyle(fontSize: 16),
+                        controller: usernameController,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(50)),
+                      child: TextField(
+                        onChanged: (text) {
+                          setState(() {});
+                        },
+                        obscureText: true,
+                        obscuringCharacter: "*",
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Utils.mainThemeColor,
+                          ),
+                          contentPadding: EdgeInsets.only(
+                            left: 15,
+                            bottom: 11,
+                            top: 11,
+                            right: 15,
+                          ),
+                          hintText: 'Password',
+                        ),
+                        controller: passwordController,
+                      ),
                     ),
                   ],
                 ),
